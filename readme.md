19d944d1ecb7446aa52099ef5c75b512f5db8732
Renato Teixeira Do Nascimento <nascimentor@brq.com>
2023-12-18 22:02:16 -0300

    adicionado readme

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Detona Ralf
+
+## Resumo
+
+Detona Ralf é um jogo de quebra-cabeça para dois jogadores. O objetivo do jogo é derrotar a parede de tijolos maligna usando diferentes habilidades e itens.
+
+[Detona Ralf]("./docs/screen-game.png")
+
+## Regras
+
+* O jogo é jogado em uma tela dividida horizontalmente. Cada jogador controla um personagem, Ralf ou Vanellope.
+* Os personagens podem se mover para a esquerda, para a direita, para cima e para baixo.
+* Os personagens podem usar itens para interagir com o ambiente.
+* A parede de tijolos maligna é a principal ameaça do jogo. Ela pode atacar os personagens com lasers e projéteis.
+
+## Habilidades e itens
+
+* **Ralf:** Ralf pode usar seu martelo para quebrar tijolos. Ele também pode usar seu gancho para pegar itens e se mover para cima.
+ 
+* **Vanellope:** Vanellope pode usar sua habilidade de correr para se mover mais rápido. Ela também pode usar seu poder de teletransporte para se mover para qualquer lugar na tela.
+[Image of Vanellope, o personagem do jogo Detona Ralf]
+
+## Objetos
+
+* **Tijolos:** Os tijolos podem ser quebrados com o martelo de Ralf ou com a habilidade de correr de Vanellope.
+* **Chaves:** As chaves abrem portas que levam a novos níveis.
+* **Moedas:** As moedas podem ser coletadas para ganhar pontos.
+
+## Vencedor
+
+O jogador que derrotar a parede de tijolos maligna primeiro vence o jogo.
+
+## Dicas
+
+* Use as habilidades e itens dos personagens para sua vantagem.
+* Não deixe que a parede de tijolos maligna o atinja.
+* Colete moedas para ganhar pontos e aumentar sua pontuação final.
---
+++ src/scripts/engine.js
@@ -0,0 +1,85 @@
+let itervalToCountDown= 0; 
+let intervalToTransition = 0;
+const state = {
+    views: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score")
+    },
+    values: {
+        resultPoint: 0,
+        actualSquare: 0,
+        lifeResult: 3,
+        timeLeftCounterDownInSeconds: 60
+    },
+    tasks: {
+        initTransitionForRalphOnSquare: () => { 
+            intervalToTransition = setInterval(randomSquare, 1 * 1000)
+        },
+        initTimeLeftCountDown: () => {
+                itervalToCountDown = setInterval(() => {
+                state.values.timeLeftCounterDownInSeconds -= 1;
+                state.views.timeLeft.textContent = state.values.timeLeftCounterDownInSeconds;
+                state.tasks.monitoringWinner(itervalToCountDown, intervalToTransition);
+            }, 1 * 1000);
+        },
+        monitoringWinner: (itervalToCountDown, intervalToTransition) => {
+            if (state.values.timeLeftCounterDownInSeconds === 0 ){
+                alert("FIM DE JOGO !!!");
+                clearInterval(itervalToCountDown);
+                clearInterval(intervalToTransition);
+                
+            } else if (state.values.resultPoint >= 30) {
+                alert("Você venceu!!!!");
+                clearInterval(itervalToCountDown);
+                clearInterval(intervalToTransition);
+            }
+        }
+       
+    }
+}
+
+const randomSquare = () => {
+    state.views.squares.forEach(square => {
+        square.classList.remove("enemy");
+    });
+
+    let randomnumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.views.squares[randomnumber];
+    state.values.actualSquare = randomSquare.id;
+    randomSquare.classList.add("enemy");
+}
+
+const playSound = (fileName) => {
+    const sound = new Audio(`./assets/sounds/${fileName}.m4a`);
+    sound.volume = 0.2;
+    sound.play();
+}
+
+const addListenerHitBox = () => {
+    state.views.squares.forEach(square => {
+        square.addEventListener("mousedown", () => {
+            //quero que minha pontuação seja somada quando clicar no square aonde o half aparece
+            if (square.id === state.values.actualSquare) {
+                state.values.resultPoint += 1;
+                state.views.score.textContent = state.values.resultPoint;
+                playSound("hit");
+            }
+            else {
+                state.values.resultPoint -= 1;
+                state.views.score.textContent = state.values.resultPoint;
+            }
+        });
+    });
+}
+
+
+
+const init = () => {
+    state.tasks.initTransitionForRalphOnSquare();
+    state.tasks.initTimeLeftCountDown();
+    addListenerHitBox();
+}
+
+init();
